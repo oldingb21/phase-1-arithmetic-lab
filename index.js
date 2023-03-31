3f19d8562edcd9a6969ff5563ed84562f3bff331
Brenden Olding <oldingb21@gmail.com>
2023-03-31 16:22:47 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
 
-// Write your code here
+let random = Math.floor(Math.random() *10) + 1;
+
+let num3 = 36;
+let num4 = 8;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,4,17,12,20,11,7);
